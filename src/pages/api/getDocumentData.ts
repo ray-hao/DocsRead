793c645bb23364bb16e0df1032fc6380992bc96f
Ray Hao <r25hao@uwaterloo.ca>
2024-09-11 02:17:08 -0400

    include openai api key in env vars

--- src/pages/api/getDocumentData.ts
+++ src/pages/api/getDocumentData.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai";
-const openai = new OpenAI();
-
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
